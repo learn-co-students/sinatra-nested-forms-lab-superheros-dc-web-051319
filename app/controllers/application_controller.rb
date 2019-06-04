3e43fa701386360b46a03851247515007b9a9450
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-03 23:58:51 -0400

    Completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, proc { File.join(root, '../views/') }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :index
+  end
 
-
+  post '/teams' do
+    @params = params
+    @team = params['team']
+    @team_members = params['team']['members']
+    erb :teams
+  end
 end
